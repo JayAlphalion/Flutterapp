f57dbf9ce082869a3401006077fcdb00189f51b7
Divyansh Bansal <divyanshbansal@Divyanshs-MacBook-Air.local>
2023-01-26 01:46:36 +0530

    dialog box

--- lib/utils/AppColors.dart
+++ lib/utils/AppColors.dart
@@ -8,4 +8,8 @@ class AppColors {
   static const Color ChatBorderColor = Color(0xff90D8FF);
   static const Color chatDateTimeColor = Colors.black54;
   static const Color greyColor = Colors.grey;
+  static const Color greenColor = Colors.green;
+  static const Color redColor = Colors.red;
+  static const Color whiteColor = Colors.white;
+  static const Color blackColor = Colors.black;
 }
